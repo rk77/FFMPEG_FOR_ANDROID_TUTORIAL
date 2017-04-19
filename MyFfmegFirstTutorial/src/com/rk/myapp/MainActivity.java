620c4aec384c7a2063f6b9371348baf1d15b49c4
xugaofeng <xugaofeng@hisense.com>
2017-04-19 17:04:38 +0800

    add jni interface

--- MyFfmegFirstTutorial/src/com/rk/myapp/MainActivity.java
+++ MyFfmegFirstTutorial/src/com/rk/myapp/MainActivity.java
@@ -12,4 +12,19 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+    static
+    {
+        System.loadLibrary("FFMpegJni");
+    }
+
+    private native void nativeInit();
+    private native void nativeSetSurface();
+    private native void nativeSetUri(String uri);
+    private native void nativeGetFrameBitmap(int frame);
+    private native void nativePrepare();
+    private native void nativeStart();
+    private native void nativePause();
+    private native void nativePlay();
+    private native void nativeSeek(int position);
 }
